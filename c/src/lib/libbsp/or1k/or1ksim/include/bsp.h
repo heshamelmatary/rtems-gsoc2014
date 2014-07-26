bb4f2d178f07ed8eaf2a31e6b0719d1c6894545f
Hesham ALMatary <heshamelmatary@gmail.com>
2014-07-26 06:15:02 +0200

    Modify bsp.h for or1ksim. Now all tests for or1ksim BSP are built sucessfully

--- c/src/lib/libbsp/or1k/or1ksim/include/bsp.h
+++ c/src/lib/libbsp/or1k/or1ksim/include/bsp.h
@@ -1,4 +1,48 @@
+/**
+ * @file
+ *
+ * @ingroup or1k_or1ksim
+ *
+ * @brief Global BSP definitions.
+ */
+
+/*
+ * Copyright (c) 2014 Hesham ALMatary
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *
+ *  http://www.rtems.org/license/LICENSE
+ *
+ */
+ 
 #ifndef LIBBSP_OR1K_OR1KSIM_H
 #define LIBBSP_OR1K_OR1KSIM_H
 
+#include <bspopts.h>
+#include <rtems.h>
+#include <rtems/console.h>
+#include <rtems/clockdrv.h>
+#include <bsp/or1ksim.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define BSP_FEATURE_IRQ_EXTENSION
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 #endif /* LIBBSP_OR1K_OR1KSIM_H */
+
+/**
+ * @defgroup or1k_or1ksim support
+ *
+ * @ingroup bsp_or1k
+ *
+ * @brief or1ksim support package
+ *
+ */
+ 
