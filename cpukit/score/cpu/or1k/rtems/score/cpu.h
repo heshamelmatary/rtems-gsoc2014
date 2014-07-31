42f521d3e0a845ca7fc3c470849cc866df19a6bf
Hesham ALMatary <heshamelmatary@gmail.com>
2014-07-31 12:29:06 +0200

    Fix interrupt handling and context switch bug. Now ticker is working
    fine, but printf, print_name, print_time are not producing output
    while printk does"
    
    Conflicts:
    	c/src/lib/libbsp/or1k/or1ksim/clock/clockdrv.c

--- cpukit/score/cpu/or1k/rtems/score/cpu.h
+++ cpukit/score/cpu/or1k/rtems/score/cpu.h
@@ -704,7 +704,6 @@ static inline uint32_t or1k_interrupt_disable( void )
     : "memory"  
   ); 
     
-  return 1;
 }
 
 static inline void or1k_interrupt_enable(uint32_t level)
@@ -720,12 +719,16 @@ static inline void or1k_interrupt_enable(uint32_t level)
   
    //level = (level > 0) 1<<2 : 0; 
    
-  __asm__ volatile(           
-    "l.mfspr %0,r0,17;"      
-    "l.ori   %0,%0,0x7;"     
-    "l.mtspr r0,%0,17;"  
-    : "=r" (sr)  
-    :: "memory"
+  /* If level is > 0, interrupts are already disabled and it could not
+   * be enabled until level = 0 
+   */
+
+    __asm__ volatile(           
+      "l.mfspr %0,r0,17;"      
+      "l.ori   %0,%0,0x7;"     
+      "l.mtspr r0,%0,17;"  
+      : "=r" (sr)  
+      :: "memory"
   );
 
 }
