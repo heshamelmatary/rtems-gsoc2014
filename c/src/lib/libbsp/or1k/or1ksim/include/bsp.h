0f562f17f2b6f49edc69ce40b651e59309e7fe98
Hesham ALMatary <heshamelmatary@gmail.com>
2014-08-13 19:40:31 +0200

    Add console driver for or1ksim BSP
    Conflicts:
    	c/src/lib/libbsp/or1k/or1ksim/include/bsp.h

---
+++ c/src/lib/libbsp/or1k/or1ksim/include/bsp.h
@@ -0,0 +1,5 @@
+#ifndef LIBBSP_OR1K_OR1KSIM_H
+#define LIBBSP_OR1K_OR1KSIM_H
+
+
+#endif /* LIBBSP_OR1K_OR1KSIM_H */
--- c/src/lib/libbsp/or1k/or1ksim/startup/bspstart.c
+++ c/src/lib/libbsp/or1k/or1ksim/startup/bspstart.c
@@ -1,6 +1,11 @@
+#include <bsp.h>
+#include <bsp/uart.h>
+#include <bsp/or1ksim.h>
+
 void bsp_start()
 {
-  rtems_or1ksim_bsp_uart_init();
-  main();
+  //rtems_or1ksim_bsp_uart_init();
+  //OR1KSIM_REG(OR1KSIM_BSP_UART_REG_INT_ENABLE) = 0xff;
+  //main();
   return 0;
 }
